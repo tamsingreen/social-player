8cb312a9d4fa4fa236c81c97235685156e87e046
Tamsin <tamsin_green@hotmail.com>
2015-01-31 12:20:20 +0000

    Merged example test

--- spec/social-player-spec.js
+++ spec/social-player-spec.js
@@ -1,23 +1,5 @@
 beforeEach(function () {
     jasmine.Ajax.install();
-    window.fbAsyncInit = function() {
-        FB.init({
-          appId      : 'your-app-id',
-          xfbml      : true,
-          version    : 'v2.1'
-        });
-    };
-
-    (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "//connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    //fbUserId = "10152948259833216";
-    facebookLogin();
-    FB.api('/10152948259833216/likes');
 });
 
 afterEach(function () {
@@ -25,17 +7,42 @@ afterEach(function () {
 });
 
 describe("Social Player", function() {
+    window.FB = {
+        api: jasmine.createSpy()
+    }
 
     describe('listLikes', function() {
+
+        it('should call displayProgrammeMetadata with a bbc PID', function () {
+            spyOn(window, 'displayProgrammeMetadata');
+
+            FB.api.and.callFake(function (url, cb) {
+                cb({
+                    data : [ { category: 'Tv show', id: '123' } ],
+                    paging: { next: false }
+                });
+            });
+
+            jasmine.Ajax.stubRequest('/programmes/123').andReturn({
+                "status": 200,
+                "contentType": "application/json",
+                "responseText": JSON.stringify({ bbcBrandPid: 456 })
+            });
+
+            listLikes();
+
+            expect(window.displayProgrammeMetadata).toHaveBeenCalledWith(456);
+        });
+
         // it('should return an empty array when user has no likes', function() {
 
         // });
-        it('should return an array of 25 when a user has 25 likes, which is the current Facebook pagination limit', function() {
-            var ajaxRequest = jasmine.Ajax.requests.mostRecent();
-            ajaxRequest.respondWith(TestResponses.listLikes.scenarioTwo);
-            collateLikes();
-            expect(fbLikes[2].id).toEqual('407214965992821');
-        });
+        // it('should return an array of 25 when a user has 25 likes, which is the current Facebook pagination limit', function() {
+        //     var ajaxRequest = jasmine.Ajax.requests.mostRecent();
+        //     ajaxRequest.respondWith(TestResponses.listLikes.scenarioTwo);
+        //     collateLikes();
+        //     expect(fbLikes[2].id).toEqual('407214965992821');
+        // });
         // it('should return an array of 76 likes when a user has 76 likes, separated over 4 pages', function() {
 
         // });
