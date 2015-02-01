037f11b185e1e8bcf2ac2bbb298a6b8e55e1fe61
Tamsin <tamsin_green@hotmail.com>
2015-02-01 16:20:29 +0000

    Added better error messages and tests for ListLikes

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -17,7 +17,7 @@ function facebookLogin() {
             }, {scope: 'user_likes'});
         }
     });
-    //Handle login errors
+    //TODO: Handle login errors
 }
 
 //Request a list of the user's Facebook likes and pass them to collateLikes()
@@ -37,53 +37,71 @@ function collateLikes(response) {
     } else {
         //when complete
         searchCategories('Tv show');
+    }
+    console.log(fbLikes);
+    console.log(categoryLikes);
+}
+
+//Search the user's fbLikes for all matching the specified category (usually 'Tv show')
+function searchCategories(category) {
+    $('#loading-message').text('Searching for your favourite TV shows');
+    for (var i = 0; i < fbLikes.length; i++) {
+        if (fbLikes[i].category === category) {
+            categoryLikes.push(fbLikes[i]);
+        }
+    }
+    if (categoryLikes.length === 0) {
+        $('#spinner').toggle();
+        $('#loading-message').text('No TV programmes were found in your Facebook likes.');
+    } else {
         searchBBCProgrammes(function() {
             $('.loading').toggle();
         });
     }
 }
 
-//Search the user's fbLikes for all matching the specified category (usually 'Tv show')
-function searchCategories(category) {
-    $('#loading-message').text('Searching for your favourite TV shows');
-    for (element in fbLikes) {
-        if (fbLikes[element].category === category) {
-            categoryLikes.push(fbLikes[element]);
-        }
-    }
-}
-
 //For each programme in categoryLikes, send the id property to the /programmes endpoint, and if the result is non-null, add the BBC brand PID to bbcResults and call displayProgrammeMetadata
 function searchBBCProgrammes(callback) {
     $('#loading-message').text('Searching for your favourite TV shows from the BBC');
-    for (element in categoryLikes) {
-        $.get('/programmes/' + categoryLikes[element].id)
+    var matchingProgrammesFound = 0;
+    for (var i = 0 ; i < categoryLikes.length; i++) {
+        $.get('/programmes/' + categoryLikes[i].id)
             .done(function(data) {
                 if (data) {
                     bbcResults.push(data.bbcBrandPid);
                     displayProgrammeMetadata(data.bbcBrandPid);
+                    matchingProgrammesFound++;
                 }
         });
     }
-    callback(); //this is triggered too soon - need to trigger when last $.get is done
+    $(document).ajaxStop(function() {
+        console.log('ajaxStop triggered');
+        if (matchingProgrammesFound > 0) {
+            callback();
+        } else {
+            $('#spinner').toggle();
+            $('#loading-message').text('No iPlayer programmes matching your likes could be found.');
+            //TODO: suggest some Facebook pages to like or iPlayer programmes to watch?
+        }
+    });
 } 
 
 function displayProgrammeMetadata(bbcBrandPid) {
     var programme = $('.programme-master').html();
     $.get('http://www.bbc.co.uk/programmes/' + bbcBrandPid + '/episodes/player.json')
         .done(function(data) {
-            for (episode in data.episodes) {            
+            for (var i = 0; i < data.episodes.length; i++) {       
                 $('#programme-container').append(programme);
-                $('.programme').last().wrap("<a href='http://www.bbc.co.uk/programmes/" + data.episodes[episode].programme.pid + "'></a>");
-                $('.programme-image').last().html('<img src="http://ichef.bbci.co.uk/images/ic/150x84/' + data.episodes[episode].programme.image.pid + '.jpg">');
-                if (data.episodes[episode].programme.programme.type === "brand") {
-                    $('.programme-title').last().text(data.episodes[episode].programme.programme.title);
+                $('.programme').last().wrap("<a href='http://www.bbc.co.uk/programmes/" + data.episodes[i].programme.pid + "'></a>");
+                $('.programme-image').last().html('<img src="http://ichef.bbci.co.uk/images/ic/150x84/' + data.episodes[i].programme.image.pid + '.jpg">');
+                if (data.episodes[i].programme.programme.type === "brand") {
+                    $('.programme-title').last().text(data.episodes[i].programme.programme.title);
                 } else {
-                    $('.programme-title').last().text(data.episodes[episode].programme.programme.programme.title);
+                    $('.programme-title').last().text(data.episodes[i].programme.programme.programme.title);
                 }
-                $('.programme-episode').last().text(data.episodes[episode].programme.title);
-                $('.programme-description').last().text(data.episodes[episode].programme.short_synopsis);
-                $('.programme-availability').last().text(data.episodes[episode].programme.media.availability);
+                $('.programme-episode').last().text(data.episodes[i].programme.title);
+                $('.programme-description').last().text(data.episodes[i].programme.short_synopsis);
+                $('.programme-availability').last().text(data.episodes[i].programme.media.availability);
             }
     });
 }
--- spec/social-player-spec.js
+++ spec/social-player-spec.js
@@ -1,5 +1,5 @@
 beforeEach(function () {
-    jasmine.Ajax.install();
+  jasmine.Ajax.install();
 });
 
 afterEach(function () {
@@ -7,45 +7,88 @@ afterEach(function () {
 });
 
 describe("Social Player", function() {
-    window.FB = {
-        api: jasmine.createSpy()
-    }
+  window.FB = {
+      api: jasmine.createSpy()
+  }
 
-    describe('listLikes', function() {
+  describe('listLikes', function() {
 
-        it('should call displayProgrammeMetadata with a bbc PID', function () {
-            spyOn(window, 'displayProgrammeMetadata');
-
-            FB.api.and.callFake(function (url, cb) {
-                cb({
-                    data : [ { category: 'Tv show', id: '123' } ],
-                    paging: { next: false }
-                });
-            });
-
-            jasmine.Ajax.stubRequest('/programmes/123').andReturn({
-                "status": 200,
-                "contentType": "application/json",
-                "responseText": JSON.stringify({ bbcBrandPid: 456 })
-            });
-
-            listLikes();
-
-            expect(window.displayProgrammeMetadata).toHaveBeenCalledWith(456);
-        });
-
-        // it('should return an empty array when user has no likes', function() {
-
-        // });
-        // it('should return an array of 25 when a user has 25 likes, which is the current Facebook pagination limit', function() {
-        //     var ajaxRequest = jasmine.Ajax.requests.mostRecent();
-        //     ajaxRequest.respondWith(TestResponses.listLikes.scenarioTwo);
-        //     collateLikes();
-        //     expect(fbLikes[2].id).toEqual('407214965992821');
-        // });
-        // it('should return an array of 76 likes when a user has 76 likes, separated over 4 pages', function() {
-
-        // });
+    beforeEach(function() {
+      fbLikes = [];
+      categoryLikes = [];
     });
+
+    it('should call displayProgrammeMetadata with a bbc PID', function () {
+      spyOn(window, 'displayProgrammeMetadata');
+
+      FB.api.and.callFake(function (url, cb) {
+        cb({
+          data : [ { category: 'Tv show', id: '123' } ],
+          paging: { next: false }
+        });
+      });
+
+      jasmine.Ajax.stubRequest('/programmes/123').andReturn({
+        "status": 200,
+        "contentType": "application/json",
+        "responseText": JSON.stringify({ bbcBrandPid: 456 })
+      });
+
+      listLikes();
+
+      expect(window.displayProgrammeMetadata).toHaveBeenCalledWith(456);
+    });
+
+    it('should display a user-friendly message when likes are retrieved but no matching likes are found', function() {
+      setFixtures(sandbox({id: 'loading-message'}));
+
+      spyOn(window, 'displayProgrammeMetadata');
+
+      FB.api.and.callFake(function (url, cb) {
+        cb({
+          data : [ { category: 'Tv show', id: '789' } ],
+          paging: { next: false }
+        });
+      });
+
+      jasmine.Ajax.stubRequest('/programmes/789').andReturn({
+        "status": 200,
+        "contentType": "application/json",
+        "responseText": null
+      });
+
+      listLikes();
+      
+      jQuery.event.trigger( "ajaxStop" );
+
+      expect($('#loading-message').text()).toEqual('No iPlayer programmes matching your likes could be found.');
+    });
+
+    it('should display a user-friendly message when no user likes are found', function() {
+      setFixtures(sandbox({id: 'loading-message'}));
+
+      spyOn(window, 'displayProgrammeMetadata');
+      
+      FB.api.and.callFake(function (url, cb) {
+        cb({
+          data : [ { category: 'Musician/band', id: '789' } ],
+          paging: { next: false }
+        });
+      });
+
+      // jasmine.Ajax.stubRequest('/programmes/789').andReturn({
+      //   "status": 200,
+      //   "contentType": "application/json",
+      //   "responseText": null
+      // });
+
+      listLikes();
+      
+      //jQuery.event.trigger( "ajaxStop" );
+
+      expect($('#loading-message').text()).toEqual('No TV programmes were found in your Facebook likes.');
+    });
+
+  });
 
 });
