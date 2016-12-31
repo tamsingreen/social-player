145c0e87217c19aead03787a6a290be3b122ab27
Tamsin Green <tamsin.green@bbc.co.uk>
2016-12-31 11:20:59 +0000

    Add fixture data for index tests

---
+++ test/fixtures/episodes.js
@@ -0,0 +1,20 @@
+module.exports = {
+  episodes: [
+    {
+      programme: {
+        pid: 'somepid',
+        image: {
+          pid: 'imagepid'
+        },
+        programme: {
+          title: 'Eastenders'
+        },
+        title: 'Episode 10000',
+        short_synopsis: 'Roly goes for a haircut',
+        media: {
+          availability: '100 days left'
+        }
+      }
+    }
+  ]
+}
